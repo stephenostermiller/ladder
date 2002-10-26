9cea01a6fc3059dbc3456c7a3574195f1c4f18d5
Stephen Ostermiller <stephenostermiller@users.noreply.github.com>
2002-10-26 16:23:11 +0000

    Changed email address to contact form.

--- HighScore.java
+++ HighScore.java
@@ -1,6 +1,7 @@
 /*
  * Part of Ladder, a game.
- * Copyright (C) 1999, 2000 Stephen Ostermiller <Ladder@Ostermiller.com>
+ * Copyright (C) 1999, 2000 Stephen Ostermiller
+ * http://ostermiller.org/contact.pl?regarding=Ladder
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
